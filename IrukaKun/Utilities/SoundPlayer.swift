0255b5eef5715ece1f4eb84ab73cae56a747d089
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-03-03 21:49:17 +0900

    feat: Integrate SoundPlayer with AudioManager for click sound effects

--- IrukaKun/Utilities/SoundPlayer.swift
+++ IrukaKun/Utilities/SoundPlayer.swift
@@ -2,19 +2,11 @@ import AppKit
 
 @MainActor
 final class SoundPlayer {
-    private var clickSound: NSSound?
-
     init() {
-        if let url = Bundle.main.url(forResource: "click", withExtension: "aiff") {
-            clickSound = NSSound(contentsOf: url, byReference: true)
-        } else {
-            // Fallback: use system sound
-            clickSound = NSSound(named: "Pop")
-        }
+        // SoundPlayer now delegates to AudioManager
     }
 
     func playClick() {
-        clickSound?.stop()
-        clickSound?.play()
+        AudioManager.shared.playSoundEffect(named: "click")
     }
 }
